293d92ad118eb294357ec0a40e7da6ff6e4cf88f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-04 17:06:39 -0500

    delete list

--- app/components/trello-list.js
+++ app/components/trello-list.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete() {
+      this.$().slideUp('normal', () => {
+        this.list.deleteRecord();
+        this.list.save();
+      });
+    }
+  }
 });
