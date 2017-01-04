5314369363d04f6b8916b6c2ea443d508f2195dc
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-04 16:42:34 -0500

    create lists

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    create() {
+      const list = this.store.createRecord('list', {
+        title: this.listTitle
+      });
 
+      list.save();
+    }
+  }
 });
